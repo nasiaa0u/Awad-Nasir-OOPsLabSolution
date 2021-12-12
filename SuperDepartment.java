6e27d965c6cd16543b5e54db624d5af73d5363e4
nasiaa0u <93525279+nasiaa0u@users.noreply.github.com>
2021-12-12 08:39:25 -0800

    Delete SuperDepartment.java

--- SuperDepartment.java
+++
@@ -1,9 +0,0 @@
-package com.nasir.departments;
-
-public class SuperDepartment {
-	public void departmentName() { System.out.println("Super Department");}
-	public void getTodaysWork() { System.out.println("getTodaysWork."); }
-	public void getWorkDeadline(){ System.out.println("getWorkDeadline."); }
-	public void isTodayAHoliday(){ System.out.println("isTodayAHoliday"); }
-
-}
